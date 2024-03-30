86c6d1567180722177f5b467695ab519bd7a3a86
Mr. Abdullah <akjee204@gmail.com>
2024-03-31 03:07:10 +0500

    FIX: Model name

--- src/app/proposal-generator/page.tsx
+++ src/app/proposal-generator/page.tsx
@@ -172,7 +172,7 @@ export default function ProposalGeneration() {
                     Claude 3 Opus - High Quality
                   </SelectItem>
                   <SelectItem value={GeneratorModel.GPT_4}>
-                    ChatGPT 4 - Super Fast
+                    GPT 4 - Super Fast
                   </SelectItem>
                 </SelectContent>
               </Select>
